d3168a92485a4580e4f72d7d3605f9d1d912734d
leadedge <leadedge@adam.com.au>
2014-08-30 18:51:50 +0930

    SpoutTray added

---
+++ SPOUTTRAY/Source/resource.h
@@ -0,0 +1,15 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DLG_DIALOG                          102
+#define IDD_ABOUTBOX                            103
+#define IDM_ABOUT                               104
+#define IDC_MYICON                              105
+#define IDC_STEALTHDIALOG                       106
+#define IDI_STEALTHDLG                          107
+#define IDD_CAPSBOX								108
+// #define IDM_CAPS								109
+#define IDC_ABOUTBOXTEXT                        1000
+#define IDC_INFOTEXT                            1001
+#define IDC_CAPSTEXT                            1002
---
+++ SPOUTTRAY/Source/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#ifndef _WIN32_IE			// Allow use of features specific to IE 6.0 or later.
+#define _WIN32_IE 0x0600
+#endif
+
+#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
+
+// Windows Header Files:
+#include <windows.h>
+#include <Windowsx.h>
+#include <commctrl.h>
+#include <Shellapi.h>
+#include <Shlwapi.h>
+
+// C RunTime Header Files
+#include <stdlib.h>
+#include <malloc.h>
+#include <memory.h>
+#include <tchar.h>
