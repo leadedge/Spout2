e5841ad0d52ea1842a5aa695384e9182d8fc3d2a
leadedge <leadedge@internode.on.net>
2017-02-09 10:34:39 +1030

    VS2012 project files for SpoutPanel

---
+++ SPOUTPANEL/SpoutPanel/src/resource.h
@@ -0,0 +1,12 @@
+#ifndef IDC_STATIC
+#define IDC_STATIC (-1)
+#endif
+
+#define IDD_DIALOG1                             101
+#define IDD_DIALOG2                             102
+#define IDC_LIST1                               1000
+#define IDC_ACTIVE                              1004
+#define IDI_ICON1								1005
+#define IDC_INFO                                40001
+#define IDC_USERTEXT							40002
+#define IDC_VERS								40003
