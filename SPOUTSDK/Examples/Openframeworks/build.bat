5a153aae96132f67ecb707ee302e688aadbbd3b2
Lynn Jarvis <spout@zeal.co>
2024-08-26 11:43:39 +0930

    Update build files
    Remove "ofSpoutExample" build directory
    to trigger re-build with the new source files

--- SPOUTSDK/Examples/Openframeworks/build.bat
+++ SPOUTSDK/Examples/Openframeworks/build.bat
@@ -2,8 +2,26 @@
 rem
 rem Use MSbuild to build the example project
 rem
+rem Remove ofSpoutExample directory to use the new source files
+rmdir /s /q ofSpoutExample
+rem build the project using the new files
 msbuild.exe ofSpoutExample.sln -v:m -p:Configuration=RELEASE -p:Platform=x64
-rem
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
