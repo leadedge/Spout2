c2bcc12147711d12ace7d5f08e869d774d840f8a
Lynn Jarvis <spout.email@gmail.com>
2026-07-19 14:05:47 +0930

    Remove SpoutDX windows video example

--- SPOUTSDK/SpoutDirectX/SpoutDX/Windows/Video/DATA/FFMPEG/probe.bat
+++
@@ -1,1 +0,0 @@
-%~dp0\ffprobe.exe -v error -show_streams -of default=noprint_wrappers=1:nokey=1 -print_format ini -i %1 > "%~dp0\myprobe.ini"
--- SPOUTSDK/SpoutDirectX/SpoutDX/Windows/Video/DATA/Scripts/aa-start.vbs
+++
@@ -1,6 +0,0 @@
-Set oShell = CreateObject ("Wscript.Shell") 
-Dim strArgs
-strArgs = "cmd /c aa-video.bat"
-oShell.Run strArgs, 0, false
-
-
